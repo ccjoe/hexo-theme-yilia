04f9d9ab4b3d797f448cabbc0ab9e4e11b6ed586
ccjoe <liushaofeng@fangdd.com>
2015-08-18 17:57:32 +0800

    新增协议

--- source/js/coraui.pgg.js
+++ source/js/coraui.pgg.js
@@ -73,57 +73,61 @@
   
     //创建容器
     _showFrame = function(){
-      var $frameDom = $('<div class='+opts.guideClass.slice(1)+'><span class="switch-pos">切换显示</span></div>');
       var $placeDom = $(opts.placeDom);
-      $placeDom.prepend($frameDom);
+      $placeDom.prepend('<div class='+opts.guideClass.slice(1)+'></div>');
       var $guideDom = $placeDom.find(opts.guideClass);
-      opts.placeDom === 'body' ?
-      $guideDom.addClass('fixed').removeClass('relate') :
-      $guideDom.addClass('relate').removeClass('fixed');
 
-      $placeDom.find('.switch-pos').click(function(){
-        !$guideDom.hasClass('fixed') ?
-        $guideDom.addClass('fixed').removeClass('relate') :
-        $guideDom.addClass('relate').removeClass('fixed');
-      });
+      if(opts.placeDom === 'body'){
+        $guideDom.addClass('fixed').removeClass('relate');
+      }else{
+        $guideDom.addClass('relate').removeClass('fixed').prepend('<span class="switch-pos">切换显示</span>');
+        $placeDom.find('.switch-pos').click(function(){
+          !$guideDom.hasClass('fixed') ?
+          $guideDom.addClass('fixed').removeClass('relate') :
+          $guideDom.addClass('relate').removeClass('fixed');
+        });
+      }
     },    
-
 
     //创建锚点 是link Or Linked
     addAnchor = function(linkOrLinked){
         //遍历h1-h6
         var thishx;
-        for(var i=0; i<=5; i++){
+        //hno h的级别  h1, h2
+        //hnum h级的序号
+        for(var hno=1; hno<=6; hno++){
           //存在hx的情况
-
-          if(hxArrSet[i] > 0){
-            var j=i+1;
+          if(hxArrSet[hno-1] > 0){
+            var hsumno = 1; //同级下hx编号
             //对每一个hx遍历
-            for(var k=0; k<hxArrSet[i]; k++){
-
+            for(var hnum=1; hnum <= hxArrSet[hno-1]; hnum++){
               if(linkOrLinked === "linked"){
-
                 //创建锚点对象并添加class hx以便hx替换后a后按序查找
-                thishx = $cont.find("h"+j).eq(k).addClass("h"+j);
-                thishx.attr({"name":"to"+j+k,"id":"to"+j+k});
+                thishx = $cont.find("h"+hno).eq(hnum-1).addClass("h"+hno);
+                thishx.attr({"name":"to"+hno+hnum, "id":"to"+hno+hnum});
 
               }else if(linkOrLinked === "link"){
 
                 //创建锚点链接    hx被替换成a，导致无法查找，因此以class查找 
                 //去掉里层链接
-                thishx = $(opts.guideClass).find(".h"+j).eq(k).attr({"href":'#to'+j+k}).attr({"code-num": '#to'+j+k});
-                
-                //目录序号无0则+1； h2显示1,2,3... h3显示2.1,2.2,....
-                var m = k+1,    //序号第二位
-                    n = j-1;    //序号第一位
+                thishx = $(opts.guideClass).find(".h"+hno).eq(hnum-1).attr({"href":'#to'+hno+hnum}).attr({"code-num": '#to'+hno+hnum});
 
-                if(j === 2){          
-                    thishx.prepend('<span>'+ m +'. </span>');
-                }else if(j > 2){
-                    thishx.prepend('<span>'+ n+'.'+m +'</span>');
+                //只支持三级目录
+                //从h2起开始编号
+                if(hno === 2){          
+                    thishx.prepend('<span>'+ hnum +' </span>');
+                }else if(hno === 3){
+                    var hsub = hnum+1;
+                    //如果前面有同类则序号+1，否则；
+                    thishx.prev().hasClass('h'+hno) ? hsumno++ : null;
+                    //查找最近的上级hx
+                    var subIndex = parseInt(getClosestElemByClass(thishx, 'a.h2').find('span').text());
+                    thishx.prepend('<span>'+ subIndex + '.' + hsumno + ' </span>');
+                }else if(hno > 3){
+                    thishx.prepend('<span>• </span>');
                 }
                 //替换标签后添加class以便不影响查找
-                thishx.replaceWith('<a code-num="#to'+j+k+'" href="#to'+j+k+'" class="h'+ j +'">' + thishx.text()+ '</a>');
+                thishx.replaceWith('<a code-num="#to'+hno+hnum+'" href="#to'+hno+hnum+'" class="h'+ hno +'">' + thishx.html()+ '</a>');
           
               }
   
@@ -131,7 +135,11 @@
           }
         }         
     },
-    
+    //获取同级最近的className元素，上级用closest();
+    getClosestElemByClass = function($elem, className){
+      return !$elem.prev().hasClass(className) ? $elem.prev() : getClosestElemByClass($elem.prev(), className);
+    },
+
     //排序sort
     hxSort = function(){
       var newHxDom =$cont.find("h1,h2,h3,h4,h5,h6").clone();
@@ -144,18 +152,18 @@
     styleGuide = function(mainClass){
         var $styleDom = $('<style type="text/css"></style');    
         var guideFrame = 
-         mainClass + '{border : 1px solid #ddd; max-height : 300px; background-color : #fff; padding : 10px;  box-shadow: 0 1px 10px #999;}' +
-         mainClass + '.fixed{top:50%; right:10px;  margin-top : -150px; overflow : auto; z-index : 10000; position: fixed}' +
-         mainClass + '.relate{ position: relative}' +
+         mainClass + '{max-width:280px; background-color : #fff; padding : 10px; z-index: 10000}' +
+         mainClass + '.fixed{top:50%; right:10px;  margin-top : -150px; overflow : auto; z-index : 10000; position: fixed; box-shadow: 0 1px 10px #999; max-height : 300px;}' +
+         mainClass + '.relate{border : 2px solid #ddd; position: relative; margin:0 0 0  50px; float: right;}' +
          mainClass + ' a { display: block; font-size: 12px; color: #333; text-decoration: none; line-height : 24px; padding-left : 10px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; text-algin: left }' +
          mainClass + ' a.active { color:green }'+
-         mainClass + ' .h1{ font-size: 15px; font-weight: bold; border-bottom : 1px solid #ddd; margin-bottom: 8px; }' +
+         mainClass + ' .h1{ font-size: 15px; font-weight: bold; border-bottom : 1px solid #ddd; margin-bottom: 8px; max-width: 200px; padding-right:80px; }' +
          mainClass + ' .h2{ color: #555; font-size: 13px; font-weight:bold; margin-bottom:1px;}' +
          mainClass + ' .h3{ margin-left: 15px; color: #666; font-size: 13px;  margin-bottom:2px; }' +
          mainClass + ' .h4{ margin-left: 30px; color: #777;  margin-bottom:4px;  }' +
          mainClass + ' .h5{ margin-left: 45px; color: #999;  margin-bottom:6px;  }' +
          mainClass + ' .h6{ margin-left: 60px; color: #aaa;   }' + 
-         mainClass + ' .switch-pos{ position:absolute; right:10px; top: 5px; color: #fff; background-color:rgba(0,0,0,.5); padding: 0 5px; font-size:13px; line-height:25px; border-radius:2px;}';
+         mainClass + ' .switch-pos{ position:absolute; right:10px; top: 5px; color: #fff; background-color:rgba(0,0,0,.5); cursor: pointer; padding: 0 5px; font-size:13px; line-height:25px; border-radius:2px;}';
         //ie
         if( $styleDom[0].styleSheet ){
           $styleDom[0].styleSheet.cssText = guideFrame;
@@ -167,15 +175,20 @@
     },
 
     scrollShow = function(){
+      var $keyElem;
       //滚动时相应显示所在条目
       $(window).scroll(function(){
         $cont.find("h1,h2,h3,h4,h5,h6").each(function(i){
-          while( Math.ceil( $(this).offset().top ) - $(window).scrollTop() >= 0 ){
+          //只要滚动范围没超出最近某一个hx元素, 此hx的上一个即为正在浏览的
+          if( Math.ceil( $(this).offset().top ) - $(window).scrollTop() >= 5 ){
            //找出最小的大于0的hx元素,用href查找的话在ie7以前href会自动加上域名
-           $('a[code-num="#'+$(this).attr("name")+'"]').addClass("active").siblings().removeClass("active");
+           $keyElem = $('a[code-num="#'+$(this).attr("name")+'"]');
+           $keyElem.prev().addClass("active").siblings().removeClass("active");
            return false;
+          }else{
+            if($keyElem)
+              $keyElem.addClass("active").siblings().removeClass("active");
           }
-         
          })
       });     
     };
